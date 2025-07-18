99f0f938c11f4dad3a5492f6cb0061784356cb92
LoneMafia <69080845+LoneMafia@users.noreply.github.com>
2025-07-18 18:47:14 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 
 
 // --- Helper Functions & Constants ---
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
